ea3cb1acbc0a6e9ce09cfb48ab2afa932d2f5354
Potapova Sofia <ptv-sofia@yandex.by>
2021-03-13 22:55:39 +0300

    init: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+  let min;
+  for(var i = 0; i < array.length; i++){
+  if(min>array[i]) min=array[i];
+  return min;
+  }
+  }
+  
+  exports.max = function max (array) {
+  let max;
+  for(var i = 0; i < array.length; i++){
+  if(max<array[i]) max=array[i];
+  return max;
+  }
+  }
+  
+  exports.avg = function avg (array) {
+  var sum = 0;
+  for(var i = 0; i < array.length; i++){
+  sum += array[i];
+  }
+  let n=array.length;
+  let avg=sum/n;
+  return avg;
+  }
