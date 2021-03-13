aaaf39c309034adce7700ae9b3b77a5806d82346
Potapova Sofia <ptv-sofia@yandex.by>
2021-03-13 23:59:07 +0300

    init: index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 exports.min = function min (array) {
-  let min;
-  for(var i = 0; i < array.length; i++){
-  if(min>array[i]) min=array[i];
-  return min;
-  }
-  }
-  
-  exports.max = function max (array) {
-  let max;
-  for(var i = 0; i < array.length; i++){
-  if(max<array[i]) max=array[i];
-  return max;
-  }
-  }
-  
-  exports.avg = function avg (array) {
-  var sum = 0;
-  for(var i = 0; i < array.length; i++){
-  sum += array[i];
-  }
-  let n=array.length;
-  let avg=sum/n;
-  return avg;
-  }
+    if(array.length==0 || array==0) return 0;
+    let min=array[0];
+    for(var i = 0; i < array.length; i++){
+    if(min>array[i]) min=array[i];
+    return min;
+    }
+    }
+    
+    exports.max = function max (array) {
+    if(array.length==0 || array==0) return 0;
+    let max=array[0];
+    for(var i = 0; i < array.length; i++){
+    if(max<array[i]) max=array[i];
+    return max;
+    }
+    }
+    
+    exports.avg = function avg (array) {
+    if(array.length==0 || array==0) return 0;
+    var sum = 0;
+    for(var i = 0; i < array.length; i++){
+    sum += array[i];
+    }
+    let n=array.length;
+    let avg=sum/n;
+    return avg;
+    }
